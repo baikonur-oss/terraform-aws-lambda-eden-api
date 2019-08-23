57049f287b2ee3ea9916d5b37c0772d2c87d3454
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2019-08-23 12:48:59 +0900

    Remove .terraform files

--- .terraform/modules/34fda75a37d4cc8fcebdb106c425c61d/baikonur-oss-terraform-aws-iam-nofile-11a0b7b/output.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "IAM Role ARN"
-  value       = "${aws_iam_role.iam_role.arn}"
-}
-
-output "name" {
-  description = "IAM Role name"
-  value       = "${aws_iam_role.iam_role.name}"
-}
