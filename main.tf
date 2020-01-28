ad7afd29f890695723d64dacf30cd8fe26b2de38
Torgayev Tamirlan <torgayev_tamirlan_xb@cyberagent.co.jp>
2020-01-28 16:15:08 +0900

    fix resource names

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "eden API managed by Terraform"
 }
 
-resource "aws_api_gateway_domain_name" "example" {
+resource "aws_api_gateway_domain_name" "eden" {
   domain_name              = replace(var.api_domain_name, "/[.]$/", "")
   regional_certificate_arn = var.api_acm_certificate_arn
 
@@ -175,15 +175,15 @@ resource "aws_api_gateway_domain_name" "example" {
   }
 }
 
-resource "aws_route53_record" "example" {
-  name    = aws_api_gateway_domain_name.example.domain_name
+resource "aws_route53_record" "eden" {
+  name    = aws_api_gateway_domain_name.eden.domain_name
   type    = "A"
   zone_id = var.api_zone_id
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.example.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.example.regional_zone_id
+    name                   = aws_api_gateway_domain_name.eden.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.eden.regional_zone_id
   }
 }
 
